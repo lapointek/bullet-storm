90ae8694a7d93c2d75a9830a33c2707c3e5075ac
lapointek <lapointek.dev@gmail.com>
2025-11-08 19:59:42 -0500

    feat: add walk and idle animation to player

--- player.gd
+++ player.gd
@@ -6,3 +6,10 @@ func _physics_process(delta: float) -> void:
 	"move_up","move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()
+		
+		
